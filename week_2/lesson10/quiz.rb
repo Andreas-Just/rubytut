4ee65789397159760ce4a93ab130b145f9d2f0a7
Andrew <andreas.wq@gmail.com>
2023-02-03 12:27:21 +0100

    the quiz task has been added

--- week_2/lesson10/quiz.rb
+++ week_2/lesson10/quiz.rb
@@ -3,22 +3,43 @@ current_path = File.dirname(__FILE__)
 
 folder_path = "#{current_path}/data"
 file_name = /question_\d/
+number_questions = 3
+correct_answers = 0
 
 question_files = Dir.entries(folder_path)
 
 if Dir.exist?(folder_path)
-  puts "Папка найдена"
+  puts "Мини-викторина. Ответьте на вопросы."
 
-  random_file = question_files.select {|name| name =~ file_name}.shuffle.first(3)
-  lines = []
-  question = random_file.each do |name|
+  random_file = question_files.select {|name| name =~ file_name}.shuffle.first(number_questions)
+  questions_answers = []
+  random_file.each do |name|
     file = File.new("#{folder_path}/#{name}", "r:UTF-8")
-    lines << file.readlines
+    questions_answers << file.readlines
     file.close
   end
 
-  puts question
-  puts lines.to_s
+  questions_answers.each do | arr |
+    question, answer = arr
+    answer_options = [answer.delete("\n"), answer.delete("\n").downcase]
+
+    puts question
+
+    user_answer = nil
+
+    while user_answer.nil? || user_answer == '' do
+      user_answer = gets.chomp.strip
+    end
+
+    if answer_options.include?(user_answer)
+      correct_answers += 1
+      puts "Верный ответ!"
+    else
+      puts "Неправильно. Правильный ответ: #{answer}"
+    end
+  end
+
+  puts "Правильных ответов: #{correct_answers} из #{number_questions}"
 
 else
   puts "Папка не найдена"
