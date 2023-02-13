06bbc08d4cfd9858ca25571bd81ccb1e8fcda460
Andrew <andreas.wq@gmail.com>
2023-02-13 19:57:35 +0100

    fixed tasks `quiz` according to comments

--- week_2/lesson10/quiz.rb
+++ week_2/lesson10/quiz.rb
@@ -5,40 +5,33 @@ folder_path = "#{current_path}/data"
 file_name = "question_*.txt"
 number_questions = 3
 correct_answers = 0
+questions_answers = []
 
+puts "Папка не найдена" unless Dir.exist?(folder_path)
 
-if Dir.exist?(folder_path)
-  puts "Мини-викторина. Ответьте на вопросы."
+puts "Мини-викторина. Ответьте на вопросы."
 
-  question_files = Dir.glob("#{folder_path}/#{file_name}")
-  random_file = question_files.shuffle.first(number_questions)
-  questions_answers = []
+question_files = Dir.glob("#{folder_path}/#{file_name}")
 
-  random_file.each do |name|
-    questions_answers << File.readlines(name, chomp: true)
-  end
-
-  questions_answers.each do | arr |
-    question, answer = arr
-
-    puts question
-
-    user_answer = nil
-
-    while user_answer.nil? || user_answer == '' do
-      user_answer = gets.chomp.strip
-    end
-
-    if answer.downcase == user_answer.downcase
-      correct_answers += 1
-      puts "Верный ответ!"
-    else
-      puts "Неправильно. Правильный ответ: #{answer}"
-    end
-  end
-
-  puts "Правильных ответов: #{correct_answers} из #{number_questions}"
-
-else
-  puts "Папка не найдена"
+question_files.sample(number_questions).each do |name|
+  questions_answers << File.readlines(name, chomp: true)
 end
+
+questions_answers.each do | question, answer, hv |
+  puts question
+
+  user_answer = nil
+
+  while user_answer.nil? || user_answer == '' do
+    user_answer = gets.chomp.strip
+  end
+
+  if answer.downcase == user_answer.downcase
+    correct_answers += 1
+    puts "Верный ответ!"
+  else
+    puts "Неправильно. Правильный ответ: #{answer}"
+  end
+end
+
+puts "Правильных ответов: #{correct_answers} из #{number_questions}"
