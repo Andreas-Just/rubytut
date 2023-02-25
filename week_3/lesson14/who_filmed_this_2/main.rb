ffe2dbc5eb7a508d130b9bb6bff364bfb1e91568
Andrew <andreas.wq@gmail.com>
2023-02-25 15:52:56 +0100

    fixed task `who_filmed_this` by creating a duplicate `who_filmed_this_2` according to comments

---
+++ week_3/lesson14/who_filmed_this_2/main.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+require_relative "lib/file_reader"
+require_relative "lib/film"
+require_relative "lib/question"
+require_relative "lib/quiz"
+
+current_path = File.dirname(__FILE__)
+folder_name = "data"
+file_name = "*.txt"
+folder_path = "#{current_path}/#{folder_name}"
+number_questions = 5
+number_options = 4
+total_correct_answers = 0
+films_surveyed = []
+
+puts "Папка не найдена" unless Dir.exist?("#{current_path}/#{folder_path}")
+
+films =
+  Dir[File.join(current_path, folder_name, file_name)].map do |name|
+    title, director, year = File.readlines(name, chomp: true)
+    Film.new(title, director, year)
+  end
+
+number_questions.times do | number |
+  current_film = films.sample
+
+  while films_surveyed.include?(current_film.title)
+    current_film = films.sample
+  end
+
+  films_surveyed.push(current_film.title)
+
+  director_options =
+    films.
+      uniq(&:director).
+      sample(number_options).
+      each_with_object([current_film.director]) { | film, option |
+        option << film.director if film.director != current_film.director && option.size < number_options
+      }.
+      sample(number_options)
+
+  puts <<~CURRENT_QUIZ_STATUS
+    Вопрос #{number + 1}:
+    Кто снял «#{current_film.title}» (#{current_film.year})
+  CURRENT_QUIZ_STATUS
+  director_options.each.with_index(1) do | director, idx |
+      puts "#{idx}. #{director}"
+    end
+
+  until (user_choice = $stdin.gets.to_i).between?(1, number_options)
+    puts 'Выберите номер из предложенных вариантов'
+  end
+
+  if current_film.director == director_options[user_choice - 1]
+    total_correct_answers += 1
+    puts "Верно!"
+  else
+    puts "Неправильно! Правильный ответ: #{current_film.director}"
+  end
+end
+
+puts "Вы угадали #{total_correct_answers} из #{number_questions} режиссеров"
