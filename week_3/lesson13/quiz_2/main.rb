bcdc8d37a0521a4721b20d26a708ce5e424322e3
Andrew <andreas.wq@gmail.com>
2023-03-05 21:12:51 +0100

    fixed task quiz_2 according to comments

--- week_3/lesson13/quiz_2/main.rb
+++ week_3/lesson13/quiz_2/main.rb
@@ -3,14 +3,9 @@ require_relative "lib/question"
 require_relative "lib/quiz"
 
 current_path = File.dirname(__FILE__)
-folder_name = "data"
-file_name = "question_*.txt"
-folder_path = "#{current_path}/#{folder_name}"
-
-puts "Папка не найдена" unless Dir.exist?("#{current_path}/#{folder_path}")
 
 questions =
-  Dir[File.join(current_path, folder_name, file_name)].map do |name|
+  Dir[File.join(current_path, "data", "question_*.txt")].map do |name|
     question, answer, points = File.readlines(name, chomp: true)
     Question.new(question, answer, points)
   end
@@ -18,8 +13,6 @@ questions =
 quiz = Quiz.new(questions)
 
 until quiz.over?
-  quiz.current_step
-
   puts quiz.current_question
 
   user_answer = gets.chomp.strip.downcase
