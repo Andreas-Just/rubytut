86ca92bcf7e2e92bb1ca12b441fc18ee8a02a07e
Andrew <andreas.wq@gmail.com>
2023-03-05 20:47:45 +0100

    fixed task `who_filmed_this_2`: 1. by creating a duplicate `who_filmed_this_3` according to comments 2. films added, all films by a maximum of 5 directors

--- week_3/lesson14/who_filmed_this_3/main.rb
+++ week_3/lesson14/who_filmed_this_3/main.rb
@@ -2,56 +2,44 @@
 require_relative "lib/film"
 
 current_path = File.dirname(__FILE__)
-folder_name = "data"
-file_name = "*.txt"
-folder_path = "#{current_path}/#{folder_name}"
-number_questions = 5
-number_options = 4
-total_correct_answers = 0
-films_surveyed = []
 
-puts "Папка не найдена" unless Dir.exist?("#{current_path}/#{folder_path}")
+QUESTIONS_COUNT = 3
+OPTIONS_COUNT = 4
 
 films =
-  Dir[File.join(current_path, folder_name, file_name)].map do |name|
+  Dir[File.join(current_path, "data", "*.txt")].map do |name|
     title, director, year = File.readlines(name, chomp: true)
     Film.new(title, director, year)
   end
 
-number_questions.times do | number |
-  current_film = films.sample
+films_for_quiz = films.sample(QUESTIONS_COUNT)
+all_directors = films.map(&:director).uniq
 
-  while films_surveyed.include?(current_film.title)
-    current_film = films.sample
-  end
+total_correct_answers = 0
 
-  films_surveyed.push(current_film.title)
+films_for_quiz.each.with_index(1) do | film, number |
 
-  director_options =
-    films.
-      uniq(&:director).
-      sample(number_options).
-      each_with_object([current_film.director]) { | film, option |
-        option << film.director if film.director != current_film.director && option.size < number_options
-      }.shuffle
+  director_options = all_directors.grep_v(film.director).sample(OPTIONS_COUNT - 1)
+  director_options << film.director
+  director_options.shuffle!
 
   puts <<~CURRENT_QUIZ_STATUS
     Вопрос #{number + 1}:
-    Кто снял «#{current_film.title}» (#{current_film.year})
-    #{director_options.map.with_index(1) { |director, idx| "#{idx}. #{director}"}.join("\n")}
+    Кто снял «#{film.title}» (#{film.year})
+    #{director_options.map.with_index(1) { |director, i| "#{i}. #{director}"}.join("\n")}
   CURRENT_QUIZ_STATUS
 
 
-  until (user_choice = $stdin.gets.to_i).between?(1, number_options)
+  until (user_choice = $stdin.gets.to_i).between?(1, director_options.size)
     puts 'Выберите номер из предложенных вариантов'
   end
 
-  if current_film.director == director_options[user_choice - 1]
+  if film.director == director_options[user_choice - 1]
     total_correct_answers += 1
     puts "Верно!"
   else
-    puts "Неправильно! Правильный ответ: #{current_film.director}"
+    puts "Неправильно! Правильный ответ: #{film.director}"
   end
 end
 
-puts "Вы угадали #{total_correct_answers} из #{number_questions} режиссеров"
+puts "Вы угадали #{total_correct_answers} из #{films_for_quiz.size} режиссеров"
