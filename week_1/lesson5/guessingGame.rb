4c4c7efc279136e7127cd6c1f85c227d66a811b9
Andrew <andreas.wq@gmail.com>
2023-03-12 21:24:49 +0100

    guessingGame.rb

--- week_1/lesson5/guessingGame.rb
+++ week_1/lesson5/guessingGame.rb
@@ -1,29 +1,21 @@
-number = rand(16)
+end_of_range = 15
+number = rand(end_of_range + 1)
 answers = {
-  :hot => 'Горячо ',
-  :warmth => 'Тепло ',
-  :cold => 'Холодно ',
-  :need_more => '(нужно больше)',
-  :need_less => '(нужно меньше)',
-  :congratulation => 'Поздравляю Вы угадали!'
+  hot: 'Горячо ',
+  warmth: 'Тепло ',
+  cold: 'Холодно ',
+  need_more: '(нужно больше)',
+  need_less: '(нужно меньше)',
+  congratulation: 'Поздравляю Вы угадали!'
 }
 
 puts 'Загадано число от 0 до 15, отгадайте какое?'
 
 3.times do
+  attempt = Integer(STDIN.gets, 10, exception: false)
 
-  # attempt = nil
-  #
-  # while attempt == '' || attempt != attempt.to_i.to_s do
-  #   attempt = gets.chomp
-  # end
-  #
-  # attempt = attempt.to_i
-
-  attempt = Integer(gets, exception: false)
-
-  if attempt.nil?
-    puts 'Вы должны выбрать номер'
+  if attempt.nil? || !attempt.to_i.between?(0, end_of_range)
+    puts 'Выберите номер из заданного диапазона'
     redo
   end
 
@@ -42,40 +34,6 @@ puts 'Загадано число от 0 до 15, отгадайте какое?
       puts answers[:cold] + (deviation_side ? answers[:need_less] : answers[:need_more])
     end
   end
-
-  attempt = gets.to_i
-  if attempt == number
-    puts answers[:congratulation]
-    exit
-  else
-    deviation_range = (attempt - number)
-    deviation_side = deviation_range.positive?
-
-    if deviation_range.abs == 1
-      puts answers[:hot] + (deviation_side ? answers[:need_less] : answers[:need_more])
-    elsif (2..5).include?(deviation_range.abs)
-      puts answers[:warmth] + (deviation_side ? answers[:need_less] : answers[:need_more])
-    else
-      puts answers[:cold] + (deviation_side ? answers[:need_less] : answers[:need_more])
-    end
-  end
-
-  attempt = gets.to_i
-  if attempt == number
-    puts answers[:congratulation]
-    exit
-  else
-    deviation_range = (attempt - number)
-    deviation_side = deviation_range.positive?
-
-    if deviation_range.abs == 1
-      puts answers[:hot] + (deviation_side ? answers[:need_less] : answers[:need_more])
-    elsif (2..5).include?(deviation_range.abs)
-      puts answers[:warmth] + (deviation_side ? answers[:need_less] : answers[:need_more])
-    else
-      puts answers[:cold] + (deviation_side ? answers[:need_less] : answers[:need_more])
-    end
-  end
-
-  puts 'В этот раз вам не повезло. Было загадано число ' + number.to_s
 end
+
+puts 'В этот раз вам не повезло. Было загадано число ' + number.to_s
