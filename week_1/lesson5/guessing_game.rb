993fbbd0cd17537d565d4a4848591541d485fee5
Andrew <andreas.wq@gmail.com>
2023-03-19 02:36:29 +0100

    final fixed task `guessing_game` according to comments

--- week_1/lesson5/guessing_game.rb
+++ week_1/lesson5/guessing_game.rb
@@ -1,5 +1,7 @@
-end_of_range = 15
-number = rand(end_of_range + 1)
+min_number = 0
+max_number = 15
+guessed_number_range = min_number..max_number
+guessed_number = rand(guessed_number_range)
 answers = {
   hot: "Горячо ",
   warmth: "Тепло ",
@@ -9,33 +11,33 @@ answers = {
   congratulation: "Поздравляю Вы угадали!"
 }
 
-puts "Загадано число от 0 до #{end_of_range}, отгадайте какое?"
+puts "Загадано число от #{min_number} до #{max_number}, отгадайте какое?"
 
 3.times do
-  attempt = Integer(STDIN.gets, 10, exception: false)
+  user_number = Integer(STDIN.gets, 10, exception: false)
 
-  if attempt.nil? || !attempt.to_i.between?(0, end_of_range)
+  unless guessed_number_range.include?(user_number)
     puts "Выберите номер из заданного диапазона"
     redo
   end
 
-  deviation_range = attempt - number
+  deviation_range = user_number - guessed_number
   deviation_side = deviation_range.positive?
   direction_designator = deviation_side ? answers[:need_less] : answers[:need_more]
 
-  if attempt == number
+  if user_number == guessed_number
     puts answers[:congratulation]
     exit
   end
 
-  if deviation_range.abs == 1
-    puts answers[:hot] + direction_designator
-  elsif (2..5).include?(deviation_range.abs)
-    puts answers[:warmth] + direction_designator
-  else
-    puts answers[:cold] + direction_designator
-  end
+  proximity =
+    case deviation_range.abs
+    when 1 then answers[:hot]
+    when 2..5 then answers[:warmth]
+    else answers[:cold]
+    end
 
+  puts "#{proximity} #{direction_designator}"
 end
 
-puts "В этот раз вам не повезло. Было загадано число #{number.to_s}"
+puts "В этот раз вам не повезло. Было загадано число #{guessed_number.to_s}"
