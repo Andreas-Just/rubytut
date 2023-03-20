246224cc8dbb89d5da16abd4bde36915458df463
Andrew <andreas.wq@gmail.com>
2023-03-20 15:54:36 +0100

    task `quiz` with `XML` had been added

--- week_6/lesson28/quiz/lib/quiz.rb
+++ week_6/lesson28/quiz/lib/quiz.rb
@@ -1,11 +1,16 @@
 class Quiz
-  NUMBER_QUESTIONS = 5
+  QUESTIONS_COUNT = 5
+  OPTIONS_COUNT = 4
 
+  attr_reader :options, :questions, :current_question_number, :total_correct_answers, :total_points
   def initialize(questions)
-    @questions = questions
+    @questions = questions.sample(QUESTIONS_COUNT)
+    @answers = questions.map(&:answer)
     @current_question_number = 0
     @total_points = 0
     @total_correct_answers = 0
+    @options = []
+    @start_time = Time.now
   end
 
   def next_question!
@@ -13,29 +18,37 @@ class Quiz
   end
 
   def correct_answer
-    "Неправильно. Правильный ответ: #{@questions[@current_question_number].answer}"
+    current_question.answer
   end
 
   def score_up!
-    @total_points += @questions[@current_question_number].points
+    @total_points += current_question.points
     @total_correct_answers += 1
-    "Верный ответ!"
   end
 
-  def answer_correct?(word)
-    word.strip.downcase == @questions[@current_question_number].answer.downcase
+  def answer_correct?(number)
+    @options[number - 1] == correct_answer
   end
 
   def current_question
-    @questions[@current_question_number].question
+    @questions[@current_question_number]
+  end
+
+  def current_options!
+    @options = @answers.grep_v(correct_answer).sample(OPTIONS_COUNT - 1)
+    @options << correct_answer
+    @options.shuffle!.map.with_index(1) { |answer, i| "#{i}. #{answer}"}.join("\n")
+  end
+
+  def start_time!
+    @start_time = Time.now
+  end
+
+  def time_limit?
+    Time.now - @start_time > 20
   end
 
   def over?
-    NUMBER_QUESTIONS == @current_question_number
-  end
-
-  def summarize
-    "Правильных ответов: #{@total_correct_answers} из #{NUMBER_QUESTIONS}\n" +
-    "Вы набрали #{@total_points} баллов"
+    QUESTIONS_COUNT == @current_question_number
   end
 end
--- week_6/lesson28/quiz/main.rb
+++ week_6/lesson28/quiz/main.rb
@@ -1,29 +1,62 @@
 # frozen_string_literal: true
+require 'rexml/document'
 require_relative "lib/question"
 require_relative "lib/quiz"
 
 current_path = File.dirname(__FILE__)
+file_name = "#{current_path}/data/questions.xml"
 
-questions =
-  Dir[File.join(current_path, "data", "question_*.txt")].map do |name|
-    question, answer, points = File.readlines(name, chomp: true)
-    Question.new(question, answer, points)
-  end
+unless File.exist?(file_name)
+  abort "Извиняемся, хозяин, файл #{file_name} не найден."
+end
+
+file = File.new(file_name)
+doc = REXML::Document.new(file)
+file.close
+
+questions = []
+doc.elements.each('questions/question') do |item|
+  question = item.text.strip
+  answer = item.attributes['answer']
+  points = item.attributes['points']
+  questions << Question.new(question, answer, points)
+end
 
 quiz = Quiz.new(questions)
 
 until quiz.over?
-  puts quiz.current_question
+  if quiz.time_limit?
+    puts 'Не уложились! Идите учиться!!!'
+    exit
+  end
+  quiz.start_time!
 
-  user_answer = gets.chomp
+  puts <<~CURRENT_QUIZ_STATUS
+    Вопрос #{quiz.current_question_number + 1}:
+    «#{quiz.current_question}»
+    #{quiz.current_options!}
+  CURRENT_QUIZ_STATUS
+
+  until (user_answer = $stdin.gets.to_i).between?(1, quiz.options.size)
+    puts 'Выберите номер из предложенных вариантов'
+  end
 
   if quiz.answer_correct?(user_answer)
-    puts quiz.score_up!
+    puts "Верный ответ!"
+    quiz.score_up!
   else
-    puts quiz.correct_answer
+    puts "Неправильно. Правильный ответ: #{quiz.correct_answer}"
   end
 
   quiz.next_question!
 end
 
-puts quiz.summarize
+if quiz.time_limit?
+  puts 'Не уложились! Идите учиться!!!'
+else
+  puts <<~SUMMARIZE
+    Правильных ответов: #{quiz.total_correct_answers} из #{quiz.questions.size}
+    Вы набрали #{quiz.total_points} баллов
+  SUMMARIZE
+end
+
