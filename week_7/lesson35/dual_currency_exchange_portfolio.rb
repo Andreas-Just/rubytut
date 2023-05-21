fdbd6f23bcba3a37391d52ac2d126b6a2f5f5edf
andreas-just <andreas.wq@gmail.com>
2023-05-21 22:46:59 +0200

    task `dual_currency_exchange_portfolio` fixed according to comment

--- week_7/lesson35/dual_currency_exchange_portfolio.rb
+++ week_7/lesson35/dual_currency_exchange_portfolio.rb
@@ -1,19 +1,27 @@
+# frozen_string_literal: true
+
 require 'net/http'
 require 'uri'
 require 'rexml/document'
 require 'date'
 
-uri = URI.parse('https://www.cbr.ru/scripts/XML_daily.asp')
-response = Net::HTTP.get_response(uri)
-doc = REXML::Document.new(response.body)
-value = nil
+begin
+  uri = URI.parse('https://www.cbr.ru/scripts/XML_daily.asp')
+  response = Net::HTTP.get_response(uri)
+  doc = REXML::Document.new(response.body)
+  value = nil
 
-# R01235 — Доллар США
-doc.each_element('//Valute[@ID="R01235"]') do |currency_tag|
-  value = currency_tag.get_text('Value').to_s.tr(',', '.').to_f
+  # R01235 — Доллар США
+  doc.each_element('//Valute[@ID="R01235"]') do |currency_tag|
+    value = currency_tag.get_text('Value').to_s.tr(',', '.').to_f
+  end
+
+  exchange_rate = value.to_f
+rescue
+  puts 'Не удалось получить курс доллара. Пожалуйста, введите его вручную:'
+  exchange_rate = gets.to_f
 end
 
-exchange_rate = value.to_f
 puts "Курс доллара #{exchange_rate}"
 
 puts 'Введите кол-во долларов у вас на руках:'
